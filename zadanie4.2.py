36670f87f1208de017932bcf6969ead06683a6d4
Maciej Ledzion <maciejledzion96@gmail.com>
2021-05-02 19:09:42 +0200

    Dodanie krótkiej dokumentacji

--- zadanie4.2.py
+++ zadanie4.2.py
@@ -1,7 +1,14 @@
 def is_palindrome(word):
-    print(f"My word: {word[::]}")
+    """
+    funkcja posiada zmienną "word"
+    pierwsza funckaj "print" wypisuje podane przeze mnie słowo
+    druga funkcja "print" wypisuje owrócone słowo
+    przy komendzie :return został podany warunek sprawdzenia czy podane przeze mnie słowo jest równe odwróconemu
+    wypisujemy wywołaną funkcję
+    jeśli słowa są takie same otrzymujemy True, inczej False
+    """
+    print(f"My word: {word}")
     print(f"Reverced word: {word[::-1]}")
     return word == word[::-1]
-
 
 print(is_palindrome("kajak"))
