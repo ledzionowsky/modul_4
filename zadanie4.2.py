53cb2abadee137b89bc81b2ebd3a03c48ab78138
Maciej Ledzion <maciejledzion96@gmail.com>
2021-05-03 16:10:04 +0200

    Poprawione zadanie

--- zadanie4.2.py
+++ zadanie4.2.py
@@ -1,14 +1,24 @@
-def is_palindrome(word):
+import string
+
+def is_palindrome(s):
     """
-    funkcja posiada zmienną "word"
-    pierwsza funckaj "print" wypisuje podane przeze mnie słowo
+    funkcja posiada zmienną "s"
+    metoda "lower" zmienia wszystkie litery na małe
+    metoda "replace" usuwa spacje
+    pierwsza meotda "print" wypisuje podane przeze mnie słowo
     druga funkcja "print" wypisuje owrócone słowo
     przy komendzie :return został podany warunek sprawdzenia czy podane przeze mnie słowo jest równe odwróconemu
     wypisujemy wywołaną funkcję
     jeśli słowa są takie same otrzymujemy True, inczej False
     """
-    print(f"My word: {word}")
-    print(f"Reverced word: {word[::-1]}")
-    return word == word[::-1]
 
-print(is_palindrome("kajak"))
+    s=s.lower()
+    s=s.replace(" ","")
+
+    #s=s.translate(None,string.punctuation) 
+
+    print(f"My string: {s}")
+    print(f"Reverced string: {s[::-1]}")
+    return s == s[::-1]
+
+print(is_palindrome("Kobyła ma, mały bok"))
