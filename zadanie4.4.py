e4c153e133e94610cf722f434a903b6d32d6e49c
Maciej Ledzion <maciejledzion96@gmail.com>
2021-05-22 12:16:55 +0200

    zmiany zadanie 4.4.py

--- zadanie4.4.py
+++ zadanie4.4.py
@@ -1,38 +1,41 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-result = None
+def dodawanie(a,b):
+    return a+b
+def odejmowanie(a,b):
+    return a-b
+def mnozenie(a,b):
+    return a*b
+def dzielenie(a,b):
+    return a/b
+def dane():
+    a=input()
+    b=input()
+    return a,b
 operation = input("Podaj numer operacji jaką chcesz wykonać\n"
                   "1 - Dodawanie\n"
                   "2 - Odejmowanie\n"
                   "3 - Mnożenie\n"
                   "4 - Dzielenie\n"
                   "Twoja operacja: ")
+
+#b = float(input("Podaj liczbe: "))
+#a = float(input("Podaj liczbe: "))
+
 if operation == '1':
-    logging.info("Wybrałeś dodawanie")
-    num1 = float(input("pierwszy składnik dodawania: "))
-    num2 = float(input("drugi składnik dodawania: "))
-    result = num1+num2
+
+    logging.info("Wybrałeś dodawanie. Wynik jest równy: "+str(dodawanie(a,b)))
 elif operation == '2':
-    logging.info("Wybrałeś odejmowanie")
-    num1 = float(input("Odjemna: "))
-    num2 = float(input("Odjemnik: "))
-    result =num1-num2
+    logging.info("Wybrałeś odejmowanie. Wynik jest równy: "+str(odejmowanie(a,b)))
 elif operation == '3':
-    logging.info("Wybrałeś mnożenie")
-    num1 = float(input("Pierwszy czynnik: "))
-    num2 = float(input("Drugi czynnik: "))
-    result = num1*num2
+    logging.info("Wybrałeś mnożenie. Wynik jest równy: "+str(mnozenie(a,b)))
 elif operation == '4':
-    logging.info("Wybrałeś dzielenie")
-    num1 = float(input("Dzielna: "))
-    num2 = float(input("Dzielnik: "))
-    if num2>0:
-        result =num1/num2
+    if b!=0:
+        logging.info("Wybrałeś dzielenie. Wynik jest równy: "+str(dzielenie(a,b)))
     else:
         logging.info("Pamiętaj cholero nie dziel przez zero")
         exit(1)
 else:
     logging.info("Nie podano operacji!")
     exit(1)
-print("Wynik Twojej operacji to: %s" % str(result))
